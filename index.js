5a3a3b45b135e1f872e2cf2b665f20c690aeabb1
JonathanStewart2 <Jonathan.Stewart2@hotmail.com>
2022-10-06 09:55:42 +0100

    updating

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+'use strict';
+
 const express = require("express");
 const app = express();
-app.listen(4494);
+const ducks = []
+
+// READ
+app.get("/hello", (req, res) => {
+    res.send("Hello, World");
+})
+
+// CREATE
+app.post("/createDuck", (req, res) => {
+    ducks.push(req.body);
+    res.status(201).send(ducks);
+})
+
+// DELETE
+app.delete("/deleteDuck/:id", (req, res) => {
+    console.log("ID: ", req.params.id);
+    res.send();
+})
+
+// UPDATE
+app.patch("/updateDuck/:id", (req, res) => {
+    console.log("ID:", req.params.id);
+    console.log("QUERY", req.query);
+    res.send();
+})
+
+
+const server = app.listen(4494, () => console.log(`Server successfully started on port ${server.address().port}`));
